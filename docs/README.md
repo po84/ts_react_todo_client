75a7dc8d850bf7b8f15d7a0c4c1baca0e68e9171
Po Tong <potong616@gmail.com>
2024-06-23 11:37:42 -0700

    Minor polish to get ready for github

---
+++ docs/README.md
@@ -0,0 +1,15 @@
+# Todo Demo
+ A todo list client app built using React + TypeScript + Vite
+
+# To run
+Configure `.env` file
+
+then run
+```
+npm install
+npm run dev
+```
+
+# Screenshot
+
+![Todo App Screenschot](todo_app.png)
--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ const App: React.FC = () => {
   
   return (
     <main className='App'>
-      <h1>My Todos</h1>
+      <h1>Todos</h1>
       <AddTodo saveTodo={handleSaveTodo} />
       {todos.map((todo: ITodo) => (
         <TodoItem
